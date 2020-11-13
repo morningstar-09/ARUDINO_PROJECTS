d28a370511b0b83a0e982f4fb69b1b5678a4d5e7
morningstar-09 <70749859+morningstar-09@users.noreply.github.com>
2020-11-13 19:35:52 +0530

    Update Thermistor__Degree_Centigrade_.ino

--- Temperature Sensor Using Thermistor/Thermistor__Degree_Centigrade_.ino
+++ Temperature Sensor Using Thermistor/Thermistor__Degree_Centigrade_.ino
@@ -1,11 +1,14 @@
+//Code downloaded from: https://github.com/morningstar-09/ARUDINO_PROJECTS/tree/master/Temperature%20Sensor%20Using%20Thermistor
 int ThermistorPin = 0;
 int Vo;
 float R1 = 10000;
 float logR2, R2, T;
 float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+//Code downloaded from: https://github.com/morningstar-09/ARUDINO_PROJECTS/tree/master/Temperature%20Sensor%20Using%20Thermistor
 void setup() {
 Serial.begin(9600);
 }
+//Code downloaded from: https://github.com/morningstar-09/ARUDINO_PROJECTS/tree/master/Temperature%20Sensor%20Using%20Thermistor
 void loop() {
   Vo = analogRead(ThermistorPin);
   R2 = R1 * (1023.0 / (float)Vo - 1.0);
@@ -17,3 +20,4 @@ void loop() {
   Serial.println(" degree centigrade"); 
   delay(500);
 }
+//Code downloaded from: https://github.com/morningstar-09/ARUDINO_PROJECTS/tree/master/Temperature%20Sensor%20Using%20Thermistor
